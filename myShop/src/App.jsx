caa8b6469e0193269f8734f476c470f00840fab8
Aishwarya7183 <aishwarya43111@gmail.com>
2026-03-11 12:02:27 +0530

    Product component is created ,fetched datas from fakestore api

--- myShop/src/App.jsx
+++ myShop/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Products from './components/Products'
 function App() {
   return (
     <>
       <NavBar/>
       <Hero/>
+      <Products/>
       <Footer/>
     </>
   )
---
+++ myShop/src/components/Products.jsx
@@ -0,0 +1,40 @@
+import React ,{useState,useEffect}from 'react'
+import '../styles/Products.css'
+function Products() {
+   const [product, setProduct] = useState([]);
+    useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+      });
+  }, []);
+  return (
+      <main className="products" id="products">
+
+        {product.length === 0 &&(
+        <div>
+          <h2>😕 No products found</h2>
+          <p>Try searching with a different name or category.</p>
+        </div>
+        )}
+        
+        { product.map((item,index)=>(
+        <div key={item.id} className="card">
+          <img src={item.image} alt={item.title} width="150"/>
+          <h3>{item.title}</h3>
+          <p>${item.price}</p>
+          <button>Add to Cart</button>
+        </div>
+
+
+
+        ))}
+          
+          
+        
+      </main>
+  );
+}
+
+export default Products
